809e9feaa6a0d4e82b61a6ee0bc8986a3a5cb8df
unknown <nilesh@sphoorthyengg.ac.in>
2024-06-24 15:25:48 +0530

    function views added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,14 @@
+from django.db.models import F
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 
 from django.http import HttpResponse,Http404
 """from django.template import loader"""
 
-from .models import Question
+from .models import Question,Choice
+
 
 # Create your views here.
 """def index1(request):
@@ -38,8 +43,32 @@ def detail(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,"polls/detail.html",{"question":question})
 
-def results(request,question_id):
+"""def results1(request,question_id):
     response = "You are looking at qustion %s"
-    return HttpResponse("response %question_id")
-def vote(request,question_id):
-    return HttpResponse("You are voting at question %s" %question_id)
+    return HttpResponse("response %question_id")"""
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/results.html",{"question":question})
+
+"""def vote1(request,question_id):
+    return HttpResponse("You are voting at question %s" %question_id)"""
+
+def vote(request,question_id):  
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError,Choice.DoesNotExist):
+        return render( 
+            request,
+            "polls/detail.html",
+            {
+                "question":question,
+                "error_message":" You didn't select a choice",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes")+1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
+
